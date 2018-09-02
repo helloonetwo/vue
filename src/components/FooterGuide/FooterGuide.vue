695194d8ea8d398497ad8a1b19d6f411a82ccfc6
zxw <1774761877@qq.com>
2018-09-02 22:41:14 +0800

    底部导航路由跳转

--- src/components/FooterGuide/FooterGuide.vue
+++ src/components/FooterGuide/FooterGuide.vue
@@ -1,1 +1,18 @@
-Order
+<template>
+  <div>
+    FooterGuide
+  </div>
+</template>
+
+
+<script>
+export  default  {
+  
+}
+</script>
+
+
+<style>
+
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,29 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import  Vue  from  'vue'
+import  VueRouter  from  'vue-router'
+import  Home  from  '../pages/Home/Home.vue'
+import  Search  from  '../pages/Search/Search.vue'
+import  Order  from  '../pages/Order/Order.vue'
+import  Person  from  '../pages/Person/Person.vue'
 
-Vue.use(Router)
+Vue.use(VueRouter)
 
-export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+
+export  default  new  VueRouter({
+routes:  [
+{
+path:  '/', redirect:  '/home'
+},
+{
+path:  '/home', component:  Home,
+},
+{
+path:  '/search', component:  Search,
+},
+{
+path:  '/order', component:  Order,
+},
+{
+path:  '/person', component:  Person,
+}
+]
 })
