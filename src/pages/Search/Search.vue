4155f443546cf41694219ab7db42523a7407a84d
zxw <1774761877@qq.com>
2018-09-03 22:30:21 +0800

    底部的个人 订单  搜索  外卖的静态页面

--- src/pages/Search/Search.vue
+++ src/pages/Search/Search.vue
@@ -1,5 +1,15 @@
 <template>
-<div>Msite vue  template</div>
+  <section class="search">
+         <header class="header">
+           <a class="header_title">
+             <span class="header_title_text">搜索</span>
+           </a>
+         </header>
+         <form class="search_form" action="#">
+           <input type="search" name="search" placeholder="请输入商家或美食名称" class="search_input">
+           <input type="submit" name="submit" class="search_submit">
+         </form>
+       </section>
 </template>
 
 
@@ -9,6 +19,59 @@ export  default  {}
 
 
 <style  lang="stylus"  rel="stylesheet/stylus">
-
+  @import "../../common/stylus/mixins.styl"
+  .header
+    background-color #02a774
+    position fixed
+    z-index 100
+    left 0
+    top 0
+    width 100%
+    height 45px
+    .header_title
+      position absolute
+      top 50%
+      left 50%
+      transform translate(-50%, -50%)
+      width 50%
+      color #fff
+      text-align center
+      .header_title_text
+        font-size 20px
+        color #fff
+        display block
+    .header_login
+      font-size 14px
+      color #fff
+      position absolute
+      right 15px
+      top 50%
+      transform translateY(-50%)
+      .header_login_text
+        color #fff
+  .order  //订单
+         width 100%
+         .order_no_login
+           padding-top 140px
+           width 60%
+           margin 0 auto
+           text-align center
+           >img
+             display block
+             width 100%
+             height 30%
+           >h3
+             padding 10px 0
+             font-size 17px
+             color #6a6a6a
+           >button
+             display inline-block
+             background #02a774
+             font-size 14px
+             color #fff
+             border 0
+             outline none
+             border-radius 5px
+             padding 10px 20px
 
 </style>
