3e4d7b7b3d06a24eac563eac59730bc53b2f202e
zxw <1774761877@qq.com>
2018-09-06 21:02:19 +0800

    vuex

---
+++ src/store/index.js
@@ -0,0 +1,6 @@
+import  Vue  from  'Vue'
+import  Vuex from  'Vuex'
+import state from  './state'
+import actions from  './actions'
+import mutations from  './mutations'
+import getters from  './getters'
